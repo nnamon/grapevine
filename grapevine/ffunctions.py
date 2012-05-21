9c41d84d9d92a312c38d6084c43acc74fbd385c4
Hiromi-nee <kyou.wk@gmail.com>
2012-05-21 09:11:54 +0800

    Function

--- grapevine/ffunctions.py
+++ grapevine/ffunctions.py
@@ -4,11 +4,14 @@ from ctypes import *
 
 libc = cdll.LoadLibrary("libc.dylib")
 
-scallnum = 0 #syscall number
-validinput = 0 #valid input that syscall takes
 
-libc.syscall(scallnum, validinput)
+def callsys( scn, vi ):
+    scallnum = scn #syscall number
+    validinput = vi #valid input that syscall takes
 
-##example, chmod
-#15 is AUE_CHMOD in BSD/XNU, 0644 is the chmod mask thing
-libc.syscall(15, "/Users/yuuko/examples.desktop", 0644)
+    libc.syscall(scallnum, validinput)
+
+    ##example, chmod
+    #15 is AUE_CHMOD in BSD/XNU, 0644 is the chmod mask thing
+    libc.syscall(15, "/Users/yuuko/examples.desktop", 0644)
+    return 0
