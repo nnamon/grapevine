c0b4cf40216d7dae3be42b744a158dbdb821cefd
Hiromi-nee <kyou.wk@gmail.com>
2012-05-25 16:46:58 +0800

    more functions

--- test/xnufuzz.py
+++ test/xnufuzz.py
@@ -28,8 +28,33 @@ def getrand():
 
 def getnumber():
     state = random.randrange(0,5)
-    #case statement goes here
+    #case statement goes here, lol python doesnt have switch
+    if state == 0:
+	    random.seed(1)
+	    return random.randrange(2147483647)
+    elif state == 1:
+	    random.seed(1)
+	    return (0xffffff00 | (random.randrange(2147483647) % 256)) #C-like random
+    elif state == 2:
+	    return 0x8000
+    elif state == 3:
+	    return 0xffff
+    elif state == 4:
+	    return 0x80000000
 
 def getarg():
     state = random.randrange(0,5)
     b = ''
+    if state == 0:
+	    return 0x0804fd00 #userland addr
+    elif state == 1:
+	    return 0x0000a000 #unmapped addr
+    elif state == 2:
+	    return 0xc01fa0b6 #kernel addr, a guess
+    elif state == 3:
+	    return getnumber() #some number
+    elif state == 4:
+	    return getrand()
+
+def memfuzz():
+
