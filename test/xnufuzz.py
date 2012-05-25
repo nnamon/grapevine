bef2355b33299e62857c942b22795c53e8d3288a
Kanoe Hiromi <kyou.wk@gmail.com>
2012-05-26 00:24:07 +0800

    done with xnufuzz.py? probably. Needs testing

--- test/xnufuzz.py
+++ test/xnufuzz.py
@@ -1,12 +1,13 @@
 from ctypes import *
 import random
 import binascii
+from time import sleep
 libc = cdll.LoadLibrary("libc.dylib")
 
 
 #This ignore list is customized for xnu-1486.2.11 (10.6.2).
 
-ignore[] = [
+ignore = [
 	8, 11, 17, 19, 21, 22, 38, 40, 45, 62, 63, 64, 67, 
     68, 69, 70, 71, 72, 77, 84, 87, 88, 91, 94, 99, 101, 
     102, 103, 107, 108, 109, 110, 112, 113, 114 115, 119, 125, 129, 130, 
@@ -57,6 +58,27 @@ def getarg():
 	    return getrand()
 
 def memfuzz():
+    arg = []
+    syscallnr = 0
+    flag = 1
+    random.seed(getseed())
+    while True:
+        flag = 1
+        while not flag == 0:
+            flag = 0
+            syscallnr = (random.randrange(2147483647) % 253)
+            for i in ignore:
+                if(i == syscallnr):
+                    flag = 1
+                    break
+    
+        for i in range(0,8)
+           arg.append(getarg())
 
+        print('syscall({}, {}, {}, {}, {}, {}, {}, {}, {})\n').format(syscallnr, arg[0], arg[1], arg[2], arg[3], arg[4], arg[5], arg[6], arg[7])
+        sleep(5/1000000.0)
+        returnVal = libc.syscall(syscallnr, arg[0], arg[1], arg[2], arg[3], arg[4], arg[5], arg[6], arg[7])
+        print "return: ", returnVal
+        
 if __name__ == "__main__"
-  memfuzz()
+    memfuzz()
