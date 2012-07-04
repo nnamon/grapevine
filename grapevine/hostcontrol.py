342967ae5a4f2afb65c2a2fd3818fe1c345b20ae
Hiromi-nee <kyou.wk@gmail.com>
2012-07-04 13:00:33 +0800

    networking

---
+++ grapevine/hostcontrol.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+UDP_IP="127.0.0.1"
+UDP_PORT=5000
+
+def prompt():
+    sys.stdout.write( "command> ")
+    uin = raw_input()
+    return uin
+
+if __name__ == "__main__":
+    sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+    print "Fuzzd IP: ", UDP_IP
+    print "Fuzzd Port: ", UDP_PORT
+    while True:
+        msg = prompt()
+        if msg == "exit":
+            print "Exiting"
+            exit()
+        print "Send: ", msg
+        sock.sendto( msg, (UDP_IP, UDP_PORT) )
