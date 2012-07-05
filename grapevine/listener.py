6b43dd7504fd1b0aca69cc8a86e95a8a48dfe120
Hiromi-nee <kyou.wk@gmail.com>
2012-07-05 09:38:44 +0800

    classed listener

--- grapevine/listener.py
+++ grapevine/listener.py
@@ -1,11 +1,13 @@
 import socket
 
-UDP_IP="127.0.0.1"
-UDP_PORT=5000
+#UDP_IP="127.0.0.1"
+#UDP_PORT=5000
+class listener:
 
-sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-sock.bind( (UDP_IP, UDP_PORT) )
+    def spawn(ip, port):
+        sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        sock.bind( (ip, port) )
 
-while True:
-    data, addr = sock.recvfrom( 1024 )
-    print data
+        while True:
+            data, addr = sock.recvfrom( 1024 )
+            print data
